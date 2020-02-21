006a8effe892c9638b1f1d57bebcdd7c91abc8db
ttdys108 <ttdys108@163.com>
2020-02-21 13:41:51 +0800

    auth filter

---
+++ admin-service/src/main/java/com/example/admin/filter/AuthFilter.java
@@ -0,0 +1,28 @@
+package com.example.admin.filter;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@WebFilter("/*")
+public class AuthFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        String token = httpServletRequest.getHeader("Authorization");
+        if(StringUtils.isBlank(token)) {
+            httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        } else {
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+        }
+
+    }
+}
